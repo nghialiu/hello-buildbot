619e20733dacc7d72c02a1414f31b239e43dbdb2
nghialiu <nghia.liu.01@gmail.com>
2022-01-06 18:20:04 +0700

    Change newthing on main branch

--- hello/test_hello.py
+++ hello/test_hello.py
@@ -10,7 +10,11 @@ class TestHello(unittest.TestCase):
     def test_world(self):
         """sample test"""
         self.assertEqual(hello('world'), 'hello world')
+        print ("Test Main Branch")
 
     def test_world_unicode(self):
         """sample test with unicode"""
         self.assertEqual(hello(u'world'), u'hello world')
+
+if __name__=="__main__":
+    print ("Test Main Branch")
