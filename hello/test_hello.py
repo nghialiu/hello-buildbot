40d33d02b969aaf93504778371174498b4070698
nghialiu <nghia.liu.01@gmail.com>
2022-01-06 18:08:58 +0700

    newthing

--- hello/test_hello.py
+++ hello/test_hello.py
@@ -10,10 +10,12 @@ class TestHello(unittest.TestCase):
     def test_world(self):
         """sample test"""
         self.assertEqual(hello('world'), 'hello world')
+        print ("Test Hello World")
 
     def test_world_unicode(self):
         """sample test with unicode"""
         self.assertEqual(hello(u'world'), u'hello world')
+        print ("Test Hello World Unicode")
 
 if __name__=="__main__":
     print ("Test")
